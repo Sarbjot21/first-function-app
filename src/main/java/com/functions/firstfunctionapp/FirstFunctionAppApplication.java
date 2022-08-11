e464b44938ba55dacbc07bb0d5e3b499066571d3
Sarbjot Singh <srbjtsingh94@gmail.com>
2022-08-11 14:50:05 +0530

    Initial Commit

---
+++ src/main/java/com/functions/firstfunctionapp/FirstFunctionAppApplication.java
@@ -0,0 +1,13 @@
+package com.functions.firstfunctionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstFunctionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstFunctionAppApplication.class, args);
+	}
+
+}
